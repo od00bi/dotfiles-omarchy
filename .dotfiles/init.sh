7b4643621e547980f060d6f91b9e49ad5743ab01
od00bi <philip@od00bi.com>
2026-01-23 08:55:05 +0100

    simplified init script

--- .dotfiles/init.sh
+++ .dotfiles/init.sh
@@ -3,37 +3,43 @@
 dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME"
 repo="od00bi/dotfiles-omarchy.git"
 
-first_time_configuration () {
-  $dotfiles config status.showUntrackedFiles no
+prompt () {
+  local level_num=$1
+  local prompt=$2
+  case "$level_num" in
+    1) level_string="INFO" ;;
+    2) level_string="WARNING" ;;
+    3) level_string="ERROR" ;;
+    *) level_string="INFO" ;;
+  esac
+  echo "init.sh: [$level_string] $prompt"
+}
 
-  echo -n "Change repo remote to ssh?(y): "
-  read remote
-  if [ "$remote" = "y" ]; then
-    $dotfiles remote rm origin
-    $dotfiles remote add origin git@github.com:$repo
+yesorno () {
+  local prompt="$1"
+  read -p "init.sh: $prompt(y): " awnser
+  if [[ "$awnser" == "y" ]]; then
+    return 0
+  else
+    return 1
   fi
-
-  /usr/bin/notify-send "intit.sh: Configure your preferred monitor setup and exit"
-  # The monitor setup is different for every new host, but I always have to configure it once depending on scale, orientation etc
-  /usr/bin/nvim ~/.config/hypr/monitors.conf
 }
 
 if [ -d ~/.dotfiles ]; then
-  echo "Found existing dotfiles folder"
-  if ! $dotfiles pull; then
-    echo -n "Git pull failed, checkout and try again?(y): "
-    read again
-    if [ $again = "y" ]; then
-      $dotfiles checkout -f
-      $dotfiles pull
-    else
-      echo "init.sh: exiting"
-      exit 1
+  prompt 1 "Found existing dotfiles folder"
+  if yesorno "Get the latest changes from remote?"; then
+    if ! $dotfiles pull; then
+      if yesorno "Git pull failed, checkout and try again?(y)"; then
+        $dotfiles checkout -f
+        $dotfiles pull
+      else
+        prompt 2 "Exiting"
+        exit 1
+      fi
     fi
   fi
 else
-  echo "Running first-time setup"
-  FIRST_TIME=true
+  echo "init.sh: Cloning repo"
   /usr/bin/git clone --bare https://github.com/$repo ~/.dotfiles/.git
 fi
 
@@ -45,6 +51,15 @@ fi
 
 /usr/bin/ansible-playbook -c local --ask-become-pass ~/.dotfiles/general.yml
 
-if [ "$FIRST_TIME" = true ]; then
-  first_time_configuration
+$dotfiles config status.showUntrackedFiles no
+
+if yesorno "Chagne repo remote to ssh?"; then
+  $dotfiles remote rm origin
+  $dotfiles remote add origin git@github.com:$repo
 fi
+
+if yesorno "Configure monitors?"; then
+  /usr/bin/nvim ~/.config/hypr/monitors.conf
+fi
+
+prompt 1 "Setup complete"
